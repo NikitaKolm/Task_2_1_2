11d67fa498f9d981723f13e5085e0366c0483786
NikitaKolm <nkolmykov7@gmail.com>
2024-01-19 12:06:12 +0300

    Add 1. Class Cat 2. Bean cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println("helloworld beans are the same: " + (bean == bean1));
+        Cat catBean = applicationContext.getBean("cat", Cat.class);
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println("cat beans are the same: " + (catBean == catBean1));
     }
 }
